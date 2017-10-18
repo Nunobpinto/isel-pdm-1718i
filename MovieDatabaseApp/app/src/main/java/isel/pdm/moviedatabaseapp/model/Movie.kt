4154f7fa2421cad6a681a6fab7c6bfb774da4731
Nunobpinto <nunobernardopinto@gmail.com>
2017-10-18 23:42:15 +0100

    deleted project due to fatal errors

--- MovieDatabaseApp/app/src/main/java/isel/pdm/moviedatabaseapp/model/Movie.kt
+++
@@ -1,9 +0,0 @@
-package isel.pdm.moviedatabaseapp.model
-
-data class Movie (val title:String,
-                  val id : Int,
-                  val rating:Int,
-                  val date:String,
-                  val posterUrl : String,
-                  val overview : String
-                  )
