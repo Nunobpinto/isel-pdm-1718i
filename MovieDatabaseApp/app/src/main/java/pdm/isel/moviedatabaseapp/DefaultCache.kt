9390c21dfd2e1a6d468ed6947866b5ae0f519ca5
Nunobpinto <nunobernardopinto@gmail.com>
2017-10-25 16:31:43 +0100

    finished image cache

--- MovieDatabaseApp/app/src/main/java/pdm/isel/moviedatabaseapp/DefaultCache.kt
+++ MovieDatabaseApp/app/src/main/java/pdm/isel/moviedatabaseapp/DefaultCache.kt
@@ -4,7 +4,10 @@ import android.graphics.Bitmap
 import com.android.volley.toolbox.ImageLoader
 
 class DefaultCache : ImageLoader.ImageCache {
+    private val cache: HashMap<String, Bitmap> = HashMap(18)
 
-    override fun getBitmap(url: String?): Bitmap? = null
-    override fun putBitmap(url: String?, bitmap: Bitmap?) = Unit
+    override fun getBitmap(url: String?): Bitmap? = cache[url]
+    override fun putBitmap(url: String, bitmap: Bitmap)  {
+        cache.put(url, bitmap)
+    }
 }
