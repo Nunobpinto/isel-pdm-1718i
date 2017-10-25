a786a70e7a02b1275ac2d1efd01be5392d660836
brunofilipe <bruno.filipe1996@hotmail.com>
2017-10-25 16:34:12 +0100

    api_key read in MovieApplication

--- MovieDatabaseApp/app/src/main/java/pdm/isel/moviedatabaseapp/service/MovieTMDBService.kt
+++ MovieDatabaseApp/app/src/main/java/pdm/isel/moviedatabaseapp/service/MovieTMDBService.kt
@@ -7,9 +7,15 @@ import pdm.isel.moviedatabaseapp.HttpRequest
 import pdm.isel.moviedatabaseapp.MovieApplication
 import pdm.isel.moviedatabaseapp.model.dataDto.MovieDto
 import pdm.isel.moviedatabaseapp.model.dataDto.MovieListDto
+import android.R.raw
+import android.app.Application
+import android.content.res.Resources
+import pdm.isel.moviedatabaseapp.R
+import java.io.InputStream
 
-class MovieTMDBService : MovieProvider {
-    private var API_KEY = readAPIKEY()
+
+class MovieTMDBService (apikey:String): MovieProvider {
+    private var API_KEY :String = apikey
     private val MOVIES_BY_NAME_URL = "https://api.themoviedb.org/3/search/movie?api_key=$API_KEY&language=en-US&page=1&query=%s"
     private val MOVIE_DETAILS_URL = "https://api.themoviedb.org/3/movie/%d?api_key= $API_KEY&language=en-US"
     private val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=$API_KEY&language=en-US&page=1"
@@ -70,10 +76,5 @@ class MovieTMDBService : MovieProvider {
         (ctx as MovieApplication).requestQueue.add(req)
     }
 
-    private fun readAPIKEY():String{
-        //val bufferedReader = File(MovieTMDBService::class.java.getResource("/res/api_key.txt")).bufferedReader()
-        //return bufferedReader.use { it.readText() }
-        return "af53a8fb127279b18d0cdbd065d80e2d"
-    }
 
 }
